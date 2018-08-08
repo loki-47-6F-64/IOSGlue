134248adda7a59ab12171f9c522fd3f025830545
loki <loki-47-6F-64@users.noreply.github.com>
2018-08-08 21:45:52 +0200

    Initial Commit

---
+++ IOSGlue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSGlue
+//
+//  Created by Petra Farber on 08-08-18.
+//  Copyright © 2018 Loki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
